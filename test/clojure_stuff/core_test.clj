dffee9e2465b3cc4dc542bae85f6e0444438ef30
Abhishek Shrivastava <abhishek@realroi.com>
2016-05-16 23:14:20 -0700

    Initial commit

---
+++ test/clojure_stuff/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-stuff.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-stuff.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
