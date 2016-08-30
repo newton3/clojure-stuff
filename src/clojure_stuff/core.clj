93b01e01529796c34739f1286e88111b17e7b967
Abhishek Shrivastava <abhishek@realroi.com>
2016-08-29 23:33:48 -0700

    a basic http call

--- src/clojure_stuff/core.clj
+++ src/clojure_stuff/core.clj
@@ -1,7 +1,7 @@
 (ns clojure-stuff.core
-  (:gen-class))
+  (:require [clj-http.client :as client]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (client/get "http://api.github.com/users/newton3" {:as :json})))
